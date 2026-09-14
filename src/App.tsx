da6984d84857197648a6298b99ee7de5d3e4febc
raulacor <raul.acorsi@gmail.com>
2026-09-14 13:15:58 -0300

    FIX: Hero section; FIX: Modules for hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import WebThreads from './components/WebThreads/WebThreads';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero'
 import './App.css'
 
 import { IoIosArrowDown } from "react-icons/io";
@@ -10,36 +11,7 @@ export default function App() {
   return (
     <>
       <Navbar />
-      <div style={{ width: '100%', height: '800px', position: 'relative' }}>
-        <WebThreads
-          color1="#BFA181"
-          color2="#ffecaf"
-          color3="#ffffff"
-          speed={0.2}
-          threadCount={3}
-          frequency={5}
-          spread={0.18}
-          taper={1}
-          position={0.5}
-          fanMode="center"
-          glow={0.02}
-          falloff={0.6}
-          thickness={1.1}
-          brightness={0.6}
-          opacity={1}
-          mirror
-          shimmer={false}
-          grain
-          grainIntensity={0.05}
-          mouseInteraction
-          mouseStrength={0.3}
-        />
-      </div>
-      <h1>Tv Time</h1>
-      <div className='cta'>
-        <h2>cinefilar</h2>
-        <span className='cta-btn'><IoIosArrowDown size={25}/></span>
-      </div>
+      <Hero />
     </>
   )
 }
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,45 @@
+import WebThreads from '../WebThreads/WebThreads';
+import { IoIosArrowDown } from "react-icons/io";
+
+import './Hero.css'
+
+export default function Hero() {
+    return (
+        <>
+        <section className='hero-section'>
+            <div className='hero-bg'>
+            <WebThreads
+                color1="#BFA181"
+                color2="#ffecaf"
+                color3="#ffffff"
+                speed={0.2}
+                threadCount={3}
+                frequency={5}
+                spread={0.18}
+                taper={1}
+                position={0.5}
+                fanMode="center"
+                glow={0.02}
+                falloff={0.6}
+                thickness={1.1}
+                brightness={0.6}
+                opacity={1}
+                mirror
+                shimmer={false}
+                grain
+                grainIntensity={0.05}
+                mouseInteraction
+                mouseStrength={0.3}
+            />
+            </div>
+            <div className='hero-content'>
+            <h1>Tv Time</h1>
+            <div className='cta'>
+                <h2>cinefilar</h2>
+                <span className='cta-btn'><IoIosArrowDown size={25}/></span>
+            </div>
+            </div>
+        </section>
+      </>
+    )
+}
