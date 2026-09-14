804eab211caab0fabd60020a7b20327828bbbc43
raulacor <raul.acorsi@gmail.com>
2026-09-13 23:16:16 -0300

    Add: vary cool background with reactbits.dev; Add: Navbar

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import WebThreads from './components/WebThreads/WebThreads';
+import Navbar from './components/Navbar/Navbar';
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <div style={{ width: '100%', height: '800px', position: 'relative' }}>
+        <WebThreads
+          color1="#BFA181"
+          color2="#ffecaf"
+          color3="#ffffff"
+          speed={0.2}
+          threadCount={3}
+          frequency={5}
+          spread={0.18}
+          taper={1}
+          position={0.5}
+          fanMode="center"
+          glow={0.02}
+          falloff={0.6}
+          thickness={1.1}
+          brightness={0.6}
+          opacity={1}
+          mirror
+          shimmer={false}
+          grain
+          grainIntensity={0.05}
+          mouseInteraction
+          mouseStrength={0.3}
+        />
+      </div>
+    </div>
+  )
+}
